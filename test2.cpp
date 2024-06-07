d7cbfef629379280decb7e459ea9b355e21c6210
armando jose <armanditourbano@gmail.com>
2024-06-07 13:47:06 -0400

    commit inicial

---
+++ test2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+// Este es el operador de modulo: %
+
+int main () {
+    int num1;
+    int num2;
+    cout << "Ingrese el primer numero: \n";
+    cin >> num1;
+    cout << "ingrese el segundo numero: \n";
+    cin >> num2;
+    int suma ,resta , multiplicacion, resto;
+    float division;
+    suma = num1+ num2;
+    resta = num1- num2;
+    multiplicacion = num1*num2;
+    division = (float) num1 / (float) num2;
+    resto = num1%num2;
+    cout << "resultado de la suma del primer numero y el segundo numero: ";
+    cout << suma << " \n";
+    cout << "resultado de la resta del primer numero y el segundo numero: ";
+    cout << resta << "\n";
+    cout << "resultado de la multiplicacion del primer numero y el segundo numero: ";
+    cout << multiplicacion << "\n";
+    cout << "resultado de la division del primer numero y el segundo numero: ";
+    cout << division << "\n";
+    cout <<"resultado del modulo del primer numero y el segundo numero: ";
+    cout << resto << "\n";
+    return 0;
+}
